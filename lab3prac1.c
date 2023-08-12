69c31a201266178f7fda1196327ce2eb253f3d4a
Md Al Amin Hossain <140754672+mdalaminhossain2108051@users.noreply.github.com>
2023-08-12 23:29:24 +0600

    Add files via upload

---
+++ lab3prac1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num33w;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+
+    if(num1 > num2 && num1>num3)
+        {
+            printf("%d is the greatest number ",num1);
+        }
+        else if(num2>num3)
+        {
+            printf("%d is the greatest number",num2);
+        }
+    else
+    {
+       printf("%d is the greatest number",num3);
+    }
+
+    return 0;
+}
