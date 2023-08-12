f7dfc74dfcaa41b447a4c6ea9c59e9c6f8d28740
Md Al Amin Hossain <140754672+mdalaminhossain2108051@users.noreply.github.com>
2023-08-12 23:28:33 +0600

    Add files via upload

---
+++ lab3exam2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int a;
+   printf("enter the value of a=");
+   scanf("%d",&a);
+   if(a%2==0)
+   {
+       printf("Given number is even");
+   }
+   else
+    printf("Given number is odd");
+   return 0;
+}
